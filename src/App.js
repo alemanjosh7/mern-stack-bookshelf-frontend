5fc04c19791ce880f2d06118c675012e545fdc12
bonini81 <andresdominguez81@gmail.com>
2020-04-16 21:55:21 -0500

    Actualizando el Booklists y Footer, cambios por bonini81

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ function App() {
       <Route path="/" exact component={BooksList} />
       <Route path="/edit/:id" component={CreateBook} />
       <Route path="/create" component={CreateBook} />
+      <Footer />
     </Router>
   );
 }
--- src/components/BooksList.js
+++ src/components/BooksList.js
@@ -3,9 +3,50 @@ import React, { Component } from 'react'
 export default class BooksList extends Component {
     render() {
         return (
-            <div>
-                Books List
-            </div>
+            <React.Fragment>
+
+                 <div class="container">
+                 <h2>Libros</h2> 
+        <div class="row" >
+              
+             <div class="col">
+               <div class="card" style={{width: "18rem;"}}>
+  <img src="#" class="card-img-top" alt="..." />
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>    
+    </div>
+    <div class="col">
+    
+    <div class="card" style={{width: "18rem;"}}>
+  <img src="#" class="card-img-top" alt="..." />
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div> 
+
+    </div>
+    <div class="col">
+      
+    <div class="card" style={{width: "18rem;"}}>
+  <img src="#" class="card-img-top" alt="..." />
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div> 
+
+    </div>
+               </div>
+
+    </div>
+               </React.Fragment>
         )
     }
 }
